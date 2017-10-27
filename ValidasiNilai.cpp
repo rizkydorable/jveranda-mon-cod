2b97580e4f413e0e555fb88e84cd6eca46a1566d
rizkydorable <33122248+rizkydorable@users.noreply.github.com>
2017-10-27 20:54:17 +0700

    Add files via upload

---
+++ ValidasiNilai.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+
+    float nilai1, nilai2, hasil;
+
+    cout << "Masukkan bilangan yang akan dibagi\t: ";
+    cin >> nilai1;
+
+    cout << "Masukkan bilangan pembagi\t\t: ";
+    cin >> nilai2;
+
+    hasil = nilai1 / nilai2;
+
+    if((nilai1 / nilai2) == hasil) {
+
+        cout << setiosflags(ios::fixed) << endl;
+        cout << setprecision(2) << "Hasil bagi\t\t\t\t: " << hasil << endl;
+    }
+
+    return 0;
+}
+
