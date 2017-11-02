35ed8a0519c425e7c52d15161983ca7d7646f4f9
rizkydorable <33122248+rizkydorable@users.noreply.github.com>
2017-11-02 19:25:49 +0700

    Add files via upload

---
+++ Faktorial.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, hasil = 0;
+
+    cout << "Masukkan bilangan : ";
+    cin >> a;
+
+    cout << a << "! = ";
+
+    for(int x = a; x > 0; x--) {
+
+        hasil += x;
+
+        if(x != 1) {
+
+            cout << x << " + ";
+
+        } else {
+
+            cout << x << " = ";
+        }
+    }
+
+    cout << hasil << endl;
+
+    return 0;
+}
+
